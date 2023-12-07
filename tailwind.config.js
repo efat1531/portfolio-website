b2613600db52e2940b25df309e88f1930ec28216
Sp3cTer <efat1531@gmail.com>
2023-12-08 05:27:04 +0600

    Adding all files

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      vsm: "410px",
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Exo\\ 2", "sans-serif"],
+      },
+      colors: {
+        darkBlueBackground: "#170550",
+        purpleBlueBackground: "#31065A",
+        brightOrange: "#F86F03",
+      },
+    },
+  },
+  plugins: [],
+};
